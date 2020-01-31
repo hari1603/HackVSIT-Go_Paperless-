634034fecb3d22743eb4edf5fb7d7e7e9ce5a903
hari1603 <aiyarhari@gmail.com>
2020-01-31 19:54:37 +0530

    login page ui

---
+++ go_paperless/public/sign_in.js
@@ -0,0 +1,36 @@
+// firebase.auth().onAuthStateChanged(function (user) {
+//   if (user) {
+//     // User is signed in.
+
+//     document.getElementById("user_div").style.display = "block";
+//     document.getElementById("login_div").style.display = "none";
+
+//     var user = firebase.auth().currentUser;
+
+//     var email_id = user.email;
+//     document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+//   }
+// });
+
+function login() {
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then(() => { 
+      window.location = "http://localhost:3000?email=" + userEmail;
+     }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      window.alert("Error : " + errorMessage);
+      // ...
+    })
+
+}
+
+function logout() {
+  firebase.auth().signOut();
+}
